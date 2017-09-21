12a270fd44d9db2430faf8480ec2ff2343a9005d
Alexandre Cesar <acmorcego@gmail.com>
2017-09-21 15:45:06 -0300

    Add methods to encode/decode requests/responses

--- vat.go
+++ vat.go
@@ -1,5 +1,11 @@
 package main
 
+import (
+	"bytes"
+	"encoding/xml"
+	"io"
+)
+
 func BuildRequest(countryCode string, vatNumber string) RequestEnvelope {
 	return RequestEnvelope{
 		Xmlns: SoapXmlns(),
@@ -11,4 +17,16 @@ func BuildRequest(countryCode string, vatNumber string) RequestEnvelope {
 			},
 		},
 	}
+}
+func EncodeRequest(request RequestEnvelope) (*bytes.Buffer, error) {
+	buffer := new(bytes.Buffer)
+	enc := xml.NewEncoder(buffer)
+	err := enc.Encode(request)
+	return buffer, err
+}
+func DecodeResponse(reader io.Reader) (ResponseEnvelope, error) {
+	var response ResponseEnvelope
+	dec := xml.NewDecoder(reader)
+	err := dec.Decode(&response)
+	return response, err
 }
